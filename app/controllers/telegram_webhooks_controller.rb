d384dc23c493d16b86501935aa31dca98458ddd1
Anton Buryka <anton.buryka@binomo.com>
2017-11-23 14:54:55 +0300

    Replace methods

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -1,29 +1,27 @@
 class TelegramWebhooksController < Telegram::Bot::UpdatesController
-  require 'open-uri'
-  require 'net/http'
-  require 'uri'
-  require 'nokogiri'
   include Telegram::Bot::UpdatesController::MessageContext
   context_to_action!
+
+  before_action :new_elcin, only: %i[test pic mus]
 
   def help(*)
     respond_with :message, text: t('.content')
   end
 
   def test(message)
-    aaa = TelegramWebhooks.tests(message)
-    respond_with :message, text: t('.content', text: aaa)
+    aaa = @elcin.tests(message)
+    respond_with :message, text: aaa
   end
 
   def pic(*args)
     Elcin.create!(zapros: args)
-    source = search_pic(args)
+    source = @elcin.search_pic(args)
     return fuckup if source.nil?
     respond_with :message, text: source
   end
 
   def mus(*args)
-    song = get_audio(args.join(' '))
+    song = @elcin.get_audio(args.join(' '))
     return fuckup if song.nil?
     respond_with :audio, audio: song, performer: @artist, title: @track
   end
@@ -44,61 +42,11 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
 
   private
 
-  def search_pic(zapros)
-    images = yandex_pic(zapros)
-    images = bing_pic(zapros) if images.size.zero?
-    images[rand(0..images.size)] if images.size > 1
-  end
-
-  def yandex_pic(zapros)
-    url = "https://yandex.ru/images/search?text=#{zapros.join('-')}&isize=medium&itype=jpg"
-    stroka = URI.encode(url)
-    html = Net::HTTP.get(URI.parse(stroka))
-    URI.extract(html).select { |l| l[/\.(?:jpe?g)\b/] }
-  end
-
-  def bing_pic(zapros)
-    url = "https://www.bing.com/images/search?q=#{zapros.join('+')}&go=Поиск"
-    stroka = URI.encode(url)
-    html = Net::HTTP.get(URI.parse(stroka))
-    URI.extract(html).select { |l| l[/\.(?:jpe?g)\b/] }
-  end
-
-  def get_audio(poisk)
-    zapros = poisk.downcase.gsub(/[!?.,"\/\\]/, ' ').split(' ')
-    url = ("https://mp3poisk.info/s/#{zapros.join('-')}")
-
-    stroka = URI.encode(url)
-    html = Net::HTTP.get(URI.parse(stroka))
-    doc = Nokogiri::HTML(html)
-
-    return nil if doc.css('.dl')[1]['href'].nil? || doc.css('.dl')[2]['href'].nil?
-
-    href = doc.css('.dl')[2]['href'] || doc.css('.dl')[1]['href']
-
-    stroka = URI.encode(href)
-    html = Net::HTTP.get(URI.parse(stroka))
-    doc = Nokogiri::HTML(html)
-
-    href = doc.css('.buttonDownload')[0]['href']
-
-    return nil if doc.css('.buttonDownload').empty? || href.nil?
-
-    title = doc.css('.buttonDownload')[0]['download']
-    @artist = doc.css('.hide')[1].css('.artist')[0].text
-    @track = doc.css('.hide')[1].css('.track')[0].text
-
-    if !File.exist?("public/songs/#{title}")
-      File.open("public/songs/#{title}", "wb") do |saved_file|
-        open(href, "rb") do |read_file|
-          saved_file.write(read_file.read)
-        end
-      end
-    end
-    File.open("public/songs/#{title}")
-  end
-
   def fuckup
     respond_with :message, text: 'Сорян, бро, ничего нет!'
+  end
+
+  def new_elcin
+    @elcin = Elcin.new
   end
 end
--- app/models/elcin.rb
+++ app/models/elcin.rb
@@ -1,5 +1,66 @@
 class Elcin < ApplicationRecord
+  require 'open-uri'
+  require 'net/http'
+  require 'uri'
+  require 'nokogiri'
+
   def tests(message)
-    respond_with :message, text: t('.content', text: message['text'])
+    message
+  end
+
+  def search_pic(zapros)
+    images = yandex_pic(zapros)
+    images = bing_pic(zapros) if images.size.zero?
+    images[rand(0..images.size)] if images.size > 1
+  end
+
+  def yandex_pic(zapros)
+    url = "https://yandex.ru/images/search?text=#{zapros.join('-')}&isize=medium&itype=jpg"
+    stroka = URI.encode(url)
+    html = Net::HTTP.get(URI.parse(stroka))
+    URI.extract(html).select { |l| l[/\.(?:jpe?g)\b/] }
+  end
+
+  def bing_pic(zapros)
+    url = "https://www.bing.com/images/search?q=#{zapros.join('+')}&go=Поиск"
+    stroka = URI.encode(url)
+    html = Net::HTTP.get(URI.parse(stroka))
+    URI.extract(html).select { |l| l[/\.(?:jpe?g)\b/] }
+  end
+
+  def get_audio(poisk)
+    zapros = poisk.downcase.gsub(/[!?.,"\/\\]/, ' ').split(' ')
+    url = ("https://mp3poisk.info/s/#{zapros.join('-')}")
+
+    stroka = URI.encode(url)
+    html = Net::HTTP.get(URI.parse(stroka))
+    doc = Nokogiri::HTML(html)
+
+    if doc.css('.dl').nil? && doc.css('.dl')[1]['href'].empty? && doc.css('.dl')[2]['href'].empty?
+      return nil
+    end
+
+    href = doc.css('.dl')[2]['href'] || doc.css('.dl')[1]['href']
+
+    stroka = URI.encode(href)
+    html = Net::HTTP.get(URI.parse(stroka))
+    doc = Nokogiri::HTML(html)
+
+    href = doc.css('.buttonDownload')[0]['href']
+
+    return nil if doc.css('.buttonDownload').empty? || href.nil?
+
+    title = doc.css('.buttonDownload')[0]['download']
+    @artist = doc.css('.hide')[1].css('.artist')[0].text
+    @track = doc.css('.hide')[1].css('.track')[0].text
+
+    if !File.exist?("public/songs/#{title}")
+      File.open("public/songs/#{title}", "wb") do |saved_file|
+        open(href, "rb") do |read_file|
+          saved_file.write(read_file.read)
+        end
+      end
+    end
+    File.open("public/songs/#{title}")
   end
 end
