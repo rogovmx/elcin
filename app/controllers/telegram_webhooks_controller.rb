7d95eca93c18592e12eeddff58a9e7bba70d35a7
Anton Buryka <anton.buryka@binomo.com>
2017-11-27 14:41:28 +0300

    test

--- app/controllers/telegram_webhooks_controller.rb
+++ app/controllers/telegram_webhooks_controller.rb
@@ -10,9 +10,7 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     params = update
     params[:args] = args
     result = Picture::Search.call(params)
-    if result.success?
-      return respond_with :message, text: result["model"].href
-    end
+    return respond_with :message, text: result['model'].href if result.success?
     fuckup
   end
 
@@ -26,25 +24,26 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   end
 
   def songs(*args)
-    artists = Song.where(author: args.join(' ').downcase).map{ |a| [{text: a.track, callback_data: a.id}] }
+    artists = Song.where(author: args.join(' ').downcase)
+                  .map { |a| [{ text: a.track, callback_data: a.id }] }
     respond_with :message, text: args.join(' ').capitalize, reply_markup: {
       inline_keyboard: artists,
       one_time_keyboard: true,
-      selective: true,
+      selective: true
     }
   end
 
   def art(*)
-    artists = Song.select(:author).uniq.map{ |a| [{text: a.author, callback_data: a.author}] }
-    respond_with :message, text: "Исполнители:", reply_markup: {
+    artists = Song.select(:author).uniq.map { |a| [{ text: a.author, callback_data: a.author }] }
+    respond_with :message, text: 'Исполнители:', reply_markup: {
       inline_keyboard: artists,
       one_time_keyboard: true,
-      selective: true,
+      selective: true
     }
   end
 
   def callback_query(data)
-    return songs(data) if data.to_i == 0
+    return songs(data) if data.to_i.zero?
     song = Song.find(data)
     respond_with :message, text: 'Отправляю...'
     respond_with :audio, audio: File.open("public/songs/#{song.filename}"),
@@ -54,21 +53,21 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
   end
 
   def message(message)
-    return pic(Elcin.last.zapros.split(' ')) if message["text"] == "еще"
+    return pic(Elcin.last.zapros.split(' ')) if message['text'] == 'еще'
     Message.create!(
-      zapros: " ",
-      href: " ",
-      data: update["message"],
-      user_id: " ",
-      chat_id: " ",
-      username: update["message"]["from"]["username"],
-      first_name: update["message"]["from"]["first_name"],
-      last_name: update["message"]["from"]["last_name"],
-      text: update["message"]["text"]
+      zapros: ' ',
+      href: ' ',
+      data: update['message'],
+      user_id: ' ',
+      chat_id: ' ',
+      username: update['message']['from']['username'],
+      first_name: update['message']['from']['first_name'],
+      last_name: update['message']['from']['last_name'],
+      text: update['message']['text']
     )
   end
 
-  def edited_message(message);end
+  def edited_message(message); end
 
   def action_missing(action, *_args)
     if command?
@@ -76,6 +75,11 @@ class TelegramWebhooksController < Telegram::Bot::UpdatesController
     else
       respond_with :message, text: t('telegram_webhooks.action_missing.feature', action: action)
     end
+  end
+
+  def cool(message)
+    if message['text'] == 'ok'
+    respond_with :message, chat_id: -1001102168560, text: 'ok'
   end
 
   private
