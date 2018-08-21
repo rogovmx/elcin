84637982d76b5e996f8fbc276aa9218b1bad8e09
Anton Buryka <anton.buryka@binomo.com>
2018-08-21 15:12:19 +0300

    test

--- app/indices/book_index.rb
+++ app/indices/book_index.rb
@@ -1,5 +1,6 @@
-ThinkingSphinx::Index.define :book, with: :active_record do
+ThinkingSphinx::Index.define :book, with: :real_time do
   indexes title, sortable: true
+  indexes author.search_name, :as => :author, :sortable => true
 
   has author_id
 end
